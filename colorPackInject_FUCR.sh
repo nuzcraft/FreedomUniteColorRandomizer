3cf88c0a1f163c14b171cb50c5b5dd90acc4048d
nuzcraft <nuzcraft@gmail.com>
2023-09-22 21:42:40 -0500

    improved colorize script

--- colorPackInject_FUCR.sh
+++ colorPackInject_FUCR.sh
@@ -7,7 +7,8 @@ do
 # echo "$id" "$file" "$directory" "$paletteSize" "$subId" "$subDirectory";
 if [[ "$directory" ]]; then
     if [[ "$paletteSize" ]]; then
-        paletteSize_cmd="-p $paletteSize "
+        # paletteSize_cmd="-p $paletteSize "
+        paletteSize_cmd =""
     else
         paletteSize_cmd=""
     fi
@@ -18,14 +19,14 @@ if [[ "$directory" ]]; then
     fi
     directory_02_cmd="$paletteSize_cmd-d $directory/002_image$subDirectory_cmd"
     printf "colorizing $directory\n"
-    python colorize.py $directory_02_cmd
+    python n_colorize.py $directory_02_cmd
     if [[ "$directory" == *"006"* ]]; then
         if [[ "$subDirectory" == *"006"* ]]; then
             subDirectory_06_cmd=" -s $subDirectory/006_image"
         else
             subDirectory_cmd=""
         fi
-        directory_06_cmd="-p 2 -d $directory/006_image$subDirectory_06_cmd"
+        directory_06_cmd="-d $directory/006_image$subDirectory_06_cmd"
         python colorize.py $directory_06_cmd
     fi
 
